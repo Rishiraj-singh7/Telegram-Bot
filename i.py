9989ef7524f8bd3a5c473f7504ccdd0a7c9f8b17
Rishiraj Singh Shekhawat <rsshekhawat795@gmail.com>
2021-09-05 21:32:28 +0530

    import functions

--- i.py
+++ i.py
@@ -2,6 +2,24 @@ import telegram.ext
 with open('token.txt' , 'r') as f:
      TOKEN = f.read()
 
+def start(update, context):
+    update.message.reply_text("hello! welcome to neuralbot!")
+
+def help(update, context):
+    update.message.reply_text("""
+    The following commands are available:
+
+    /start -> Welcome message
+    /help -> This message
+    /content -> Infomation about nueralnine content 
+    /contact -> Infomation about contact
+    """)
+
+def content(update, context):
+   update.message.reply_text("We have videos and books! Watch and read them!")
+
+def content(update, context):
+    update.message.reply_text(" You can contact Florian on the Discord server. ")
 
 updater =telegram.ext.update(TOKEN, use_context=True)
 disp = updater.dispatcher
